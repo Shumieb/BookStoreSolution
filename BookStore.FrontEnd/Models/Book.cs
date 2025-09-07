c45e0451f3c8017c40f041e710531d60ac77bb72
Shumieb <shumiebc@gmail.com>
2025-09-07 15:10:17 +0100

    Added pages for crud operations

--- BookStore.FrontEnd/Models/Book.cs
+++ BookStore.FrontEnd/Models/Book.cs
@@ -7,17 +7,28 @@ namespace BookStore.FrontEnd.Models
         public int BookId { get; set; }
 
         [Required(ErrorMessage = "The Name is required.")]
-        [StringLength(50, MinimumLength =2, ErrorMessage ="Please enter a name with 5 to 50 characters.")]
+        [StringLength(50, MinimumLength =5, ErrorMessage ="Please enter a name with 5 to 50 characters.")]
         public required string Name { get; set; }
+
+        [Required(ErrorMessage = "The Description is required.")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Please enter a description with 5 to 100 characters.")]
+        public required string Description { get; set; }
 
         [Required(ErrorMessage = "The Author is required.")]
         public int? AuthorId { get; set; }
 
         public Author? Author { get; set; }
 
+        [Required(ErrorMessage = "The Category is required.")]
+        public int? CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+
         [Required(ErrorMessage = "The Price is required.")]
         [Range(1, 1000, ErrorMessage ="Please enter a price between 1 and 1000")]
+
         public decimal Price { get; set; }
+
         public DateOnly ReleaseDate { get; set; }
     }
 }
--- BookStore.FrontEnd/Models/Category.cs
+++ BookStore.FrontEnd/Models/Category.cs
@@ -2,14 +2,18 @@
 
 namespace BookStore.FrontEnd.Models
 {
-    public class Author
+    public class Category
     {
-        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
 
         [Required(ErrorMessage = "The Name is required.")]
         [StringLength(50, MinimumLength = 5, ErrorMessage = "Please enter a name with 5 to 50 characters.")]
         public required string Name { get; set; }
 
-        public DateOnly Added { get; set; }
+        [Required(ErrorMessage = "The Description is required.")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Please enter a description with 5 to 100 characters.")]
+        public required string Description { get; set; }
+
+
     }
 }
